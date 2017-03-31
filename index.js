f2cd9f122b9d268ce5959cd11accc3c518512f35
Fabricio C Zuardi <fabricio@fabricio.org>
2017-03-30 23:32:23 -0300

    remove debug

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const regl = require('regl')();
 const identity = require('gl-mat4/identity');
 const perspective = require('gl-mat4/perspective');
 const lookAt = require('gl-mat4/lookAt');
-const debugGrid = require('./src/debugGrid');
 const drawPlayer = require('./src/player');
 
 const cameraDistance = 3;
@@ -16,7 +15,6 @@ const projection = ({ viewportWidth, viewportHeight }) =>
 
 const render = () => {
     regl.clear({ color: [0, 0, 0, 1] });
-    debugGrid({ color: [0.5, 0.5, 0.5, 1], gridSize: [10, 10]});
     drawPlayer({ color: [0.8, 0.3, 0, 1], model, view, projection });
 }
 
