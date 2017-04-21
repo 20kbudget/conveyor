5168467ad4c0b9e233c45cca875d20115849faa3
Fabricio C Zuardi <fabricio@fabricio.org>
2017-04-21 13:05:09 -0300

    movement study

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ const tracks = [
     'w,w,n,n,w,w,w,n,n,w',
     'begin,w,n,Ns,w,w,w,s,s,w,w,w,s',
     'w,nS,w,s,s,n,s,Ws,s,w,w,s,s',
-    'w,wnS'
+    'begin,w,wnS',
+    'begin',
+    'wNs'
 ];
 const track = tracks[tracks.length - 1];
 const tiles = parseTrack({ track, direction: 0 });
@@ -37,26 +39,61 @@ let state = {
     }
 };
 
-const render = () => {
+const updatePosition = ({ state, deltaT = 1 }) => {
+    const nexPosition = zipWith(
+        (p, v) => p + deltaT * v,
+        state.position,
+        state.velocity
+    );
+    return extend(state, { position: nexPosition });
+};
+
+const STEP_ANGLE = Math.PI * 1 / 4; // 45 degrees
+const CURVE_CW = true;
+const CURVE_CCW = !CURVE_CW;
+const updateVelocity = ({
+    state,
+    velocity = 1,
+    direction = [0, 0, 0],
+    sweep = CURVE_CW,
+    progress = 1.0
+}) => {
+    const angle = direction[2] * progress * STEP_ANGLE;
+    const cos = Math.cos(angle);
+    const sin = Math.sin(angle);
+    const nextVelocity = [
+        velocity * (sweep ? cos : sin),
+        velocity * (sweep ? sin : cos),
+        0
+    ];
+    return extend(state, { velocity: nextVelocity });
+};
+
+let steps = 5;
+let states = [state];
+for (let count = 0; count < steps; count ++) {
+    state = extend(state, {
+        player: updatePosition({
+            deltaT: 1,
+            state: updateVelocity({
+                state: state.player,
+                velocity: 10 / steps,
+                curve: false,
+                direction: [0, 0, 1],
+                sweep: CURVE_CW,
+                progress: count / steps
+            })
+        })
+    });
+    states.push(extend(state))
+}
+let players = states.map(state => {
     const translation = translate([], identity([]), state.player.position);
-    drawPlayer({
+    return {
         color: [0.8, 0.3, 0, 1],
         model: translation,
         view,
         projection
-    });
-};
-// regl.frame(render);
-render();
-
-// const reducers = {
-// updatePosition: (t, entityPaths, state) =>
-// entitiesPaths.reduce(path =>
-// extend(entity, {
-// position: zipWith(
-// entity.position,
-// entity.velocity,
-// (p, v) => p + t * v
-// )
-// }))
-// };
+    };
+})
+players.forEach(player => drawPlayer(player));
