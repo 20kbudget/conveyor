ac880b0baa5ecc4943932c91c6e3df3b0eca7584
Fabricio C Zuardi <fabricio@fabricio.org>
2017-05-09 09:16:12 -0300

    alternative track

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ const projection = ({ viewportWidth, viewportHeight }) =>
 
 const tracks = [
     'f,f,l,l,f,f,f,l,l,f', // simple loop
-    'f,r,r(f,f)f,r,f,f,r,f,b(l,f,f,begin)r,f' // both side options
+    'f,r,r(f,f)f,r,f,f,r,f,b(l,f,f,begin)r,f', // both side options
+    'f,r,r(f,f)f,b(l,f,f)r,f,f,r,f,b(l,f,f)r,f', // both side options
     // 'f,fr,r,f,r,r'
     // 'r(f,f)f,r,f,f,r,f,r(f,f)f,r,f,f,r,f', // player must start turned inside
     // 'l,r,f,r,r,f,l,l,f,r,r,f,r,l,l,r,r,r,l,l,r,r,r,l', // scissor
